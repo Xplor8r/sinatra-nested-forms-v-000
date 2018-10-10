5723a4e47f25af2a4d273855ab01da78c16ec2ca
Michael Pieschel <sixtyhectares@yahoo.com>
2018-10-10 02:17:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,5 +7,15 @@ module FormsLab
       erb :new
     end
 
+    post '/student' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+    Course.new(details)
+  end
+ 
+  @courses = Course.all
+ 
+  erb :student
+end
   end
 end
